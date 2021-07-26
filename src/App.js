83f948be0ba69fa00743aa6361c108881e106f0c
cyber-sergey <zenkovich_so@mail.ru>
2021-07-26 22:28:15 +0800

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -10,22 +10,9 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
       <h1 style={{marginTop: '25px'}}>Expenses Table App</h1>
       <EditableTable setShowCreateExpenseForm={setShowCreateExpenseForm} /> 
       { <CrateExpenseForm setShowCreateExpenseForm={setShowCreateExpenseForm} showCreateExpenseForm={showCreateExpenseForm} /> }
-=======
-      <h1>Expenses Table!!!</h1>
-      <h2>Для теста пушим в ветку update</h2>
-=======
-      <h1>Expenses Table</h1>
-=======
-      <h1 style={{marginTop: '25px'}}>Expenses Table App</h1>
->>>>>>> feature/updates
->>>>>>> master
-      <EditableTable /> 
->>>>>>> c00d150bcf3f4e69ec92ea0a72df8a5d697949c0
     </div>
   );
 }
