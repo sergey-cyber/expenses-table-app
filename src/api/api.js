3df82d49c72897c5ba3c3de2fd1e42888bc910d4
cyber-sergey <zenkovich_so@mail.ru>
2021-09-05 01:24:30 +0800

    Edit baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import * as axios from "axios";
 
 const instance = axios.create({ 
     /* withCredentials: true, */
-    baseURL: 'http://localhost:5000/api/' /* 'https://my-expenses-table-app.herokuapp.com/api/' */,
+    baseURL: /* 'http://localhost:5000/api/' */ 'https://my-expenses-table-app.herokuapp.com/api/',
     headers: {
         /* "Access-Control-Allow-Origin": "*" */
     }    
