5c18707c2cd30f9b2867d216742081fe7eb2927e
cyber-sergey <zenkovich_so@mail.ru>
2021-09-12 17:08:15 +0800

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import { useState } from 'react';
-import styles from './App.module.scss';
-import { CrateExpenseForm } from './components/createExpForm/create-exp-form';
-import EditableTable from './components/table';
-import 'semantic-ui-css/semantic.min.css';
-import { Footer } from './components/footer/footer';
-import { ChoosePeriod } from './components/choosePeriod/choosePeriod';
-import { ChangeDataForm } from './components/changeDataForm/change-data-form';
-import { Header } from './components/header/header';
-import { getLangLocalization } from './utilits/localization/index';
-import { useSelector } from 'react-redux';
+import { useState } from "react";
+import styles from "./App.module.scss";
+import { CrateExpenseForm } from "./components/createExpForm/create-exp-form";
+import EditableTable from "./components/table";
+import "semantic-ui-css/semantic.min.css";
+import { Footer } from "./components/footer/footer";
+import { ChoosePeriod } from "./components/choosePeriod/choosePeriod";
+import { ChangeDataForm } from "./components/changeDataForm/change-data-form";
+import { Header } from "./components/header/header";
+import { getLangLocalization } from "./utilits/localization/index";
+import { useSelector } from "react-redux";
 import { LocalizationContext } from "./utilits/hooks/useLangLoocalization";
 
 function App() {
-
-  const currentLang = useSelector((state) => state.localizationData.currentLang); 
-  const globalThemeName = useSelector((state) => state.localizationData.globalTheme);
-  const [showCreateExpenseForm, setShowCreateExpenseForm] = useState(false); 
+  const currentLang = useSelector(
+    (state) => state.localizationData.currentLang
+  );
+  const globalThemeName = useSelector(
+    (state) => state.localizationData.globalTheme
+  );
+  const [showCreateExpenseForm, setShowCreateExpenseForm] = useState(false);
 
   return (
-    <LocalizationContext.Provider value={ getLangLocalization(currentLang) }>
+    <LocalizationContext.Provider value={getLangLocalization(currentLang)}>
       <div className={`theme-${globalThemeName}`}>
         <div className={styles.App}>
           <Header />
           <ChoosePeriod />
-          <EditableTable setShowCreateExpenseForm={setShowCreateExpenseForm} /> 
-          <CrateExpenseForm setShowCreateExpenseForm={setShowCreateExpenseForm} showCreateExpenseForm={showCreateExpenseForm} />
+          <EditableTable setShowCreateExpenseForm={setShowCreateExpenseForm} />
+          <CrateExpenseForm
+            setShowCreateExpenseForm={setShowCreateExpenseForm}
+            showCreateExpenseForm={showCreateExpenseForm}
+          />
           <ChangeDataForm />
           <Footer />
         </div>
